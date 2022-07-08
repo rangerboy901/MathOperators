e6a7c08e3aef3ec9633e3ef11dd3af75c7ca7195
Joseph W. DeWeese <52636579+rangerboy901@users.noreply.github.com>
2022-07-08 14:45:22 -0500

    initial

--- js/script.js
+++ js/script.js
@@ -1,17 +1,32 @@
-
-//window.alert("JS is connected");
+//JWD:  window.alert("JS is connected");
 /*
 Joseph DeWeese
 5 July 2022
 Math Operators
  */
 
-// Create number variable
-let num1 = 5+2;
-console.log("num1 is "+num1);
+//JWD:  Create number variable
+let num1 = 5 + 2;
+console.log("num1 is " + num1);
 
-let num2 = 10-6;
-console.log("num2 is "+num2);
+let num2 = 10 - 6;
+console.log("num2 is " + num2);
+
+//JWD:  Create a total variable using the existing variables.
+let total = num1 + num2;
+console.log("The total is " + total);
+
+let num3 = 3 * 4;
+let num4 = 10 / 2;
+console.log("num3 is " + num3 + " and the value of num4 is " + num4);
+
+//JWD:  ADD 5 TO NUM1 VARIABLE
+
+num1 = num1 + 5;
+console.log("num1 is now  " + num1)
+
+total = total / num2;
+console.log("total is now " + total);
 
 
 
